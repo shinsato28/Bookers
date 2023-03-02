0595a9f4a276111bcfd46ad964f6c68186768688
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-03-02 07:02:29 +0000

    削除、作成時のエラーメッセージとフラッシュメッセージの実装まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,17 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -23,6 +25,13 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
   end
 
   private
